f7a8354a98363daadfed6503d769da12942f2e07
Michael Bragt <michaelbragt@gmail.com>
2026-01-13 23:07:15 +0100

    kamp oversigt done med team navne(joins)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.handballmanager {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.base;
 
 
     opens com.handballmanager to javafx.fxml;
