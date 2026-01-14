da7b7ebf4d79ae47120e47939aaf67048c6e042d
ElfLady <37139509+ElfLady@users.noreply.github.com>
2026-01-14 16:30:19 +0100

    Lyds branch (#5)
    * let's hope this works
    
    * Stuff from Lyds Branch
    
    * Stuff from Lyds Branch
    
    ---------
    
    Co-authored-by: Michael Bragt <michaelbragt@hotmail.com>

--- src/main/java/com/handballmanager/Page2Controller.java
+++ src/main/java/com/handballmanager/Page2Controller.java
@@ -14,6 +14,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.lang.reflect.Array;
+
 public class Page2Controller {
 
 
@@ -28,12 +30,43 @@ public class Page2Controller {
 
     public Button newMatchButton;
 
-    public void initialize() {
-        loadMatches();
-    }
+     public void setNewMatchButtonPressed(ActionEvent e){
+            Array[] hold = {"hold1", "hold2", "hold3"};
 
-    public void setNewMatchButtonPressed(ActionEvent e){
-         System.out.println("new match button pressed");
+             // ny Dialog og set title
+             Dialog<String> dialog = new Dialog<>();
+             dialog.setTitle("Ny kamp");
+
+             // lav en opret hold knap i dialog boksen (ok knap)
+             ButtonType opretBtn = new ButtonType("Start", ButtonBar.ButtonData.OK_DONE);
+             //add knappen + en cancel knap
+             dialog.getDialogPane().getButtonTypes().addAll(opretBtn, ButtonType.CANCEL);
+
+             // Lav et tesktfelt med prompt title
+             TextField holdNavn = new TextField();
+             holdNavn.setPromptText("Holdnavn");
+
+             // sæt indhold af dialog boksen
+             dialog.getDialogPane().setContent(
+                     new VBox(10, new Label("Holdnavn:"), holdNavn)
+             );
+
+             // HVIS Ok klikkes converter return værdien til en string
+             dialog.setResultConverter(button -> {
+                 if(button == opretBtn) {
+                     return holdNavn.getText();
+                 }
+                 return null;
+             });
+
+             // Vis dialogboksen og check om der er en return værdi (skriv i terminal)
+             dialog.showAndWait().ifPresent(name -> {
+                 System.out.println("Opretter hold: " + name);
+                 TeamModel teamModel = new TeamModel(name);
+                 teamDB.create(teamModel);
+                 loadTeams();
+                 // TODO: tilføj til TableView / model
+             });
     }
 
     private void loadMatches() {
