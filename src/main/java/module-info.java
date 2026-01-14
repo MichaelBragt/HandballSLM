c78b5da620e086f320fdd21b4c8348454c1c8c1b
Michael Bragt <michaelbragt@hotmail.com>
2026-01-14 10:54:02 +0100

    module removed (#2)
    Co-authored-by: Michael Bragt <michaelbragt@gmail.com>

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.handballmanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.handballmanager to javafx.fxml;
-    exports com.handballmanager;
-}
